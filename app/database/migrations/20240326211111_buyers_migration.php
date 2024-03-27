4b5e96e0df5a7407e7895782f5c3c61de9d545c2
manishperera <manishsjc12@gmail.com>
2024-03-27 22:03:32 +0530

    buyes login issue fixed

--- app/database/migrations/20240326211111_buyers_migration.php
+++ app/database/migrations/20240326211111_buyers_migration.php
@@ -17,7 +17,7 @@ final class BuyersMigration extends AbstractMigration
             ->addColumn('user_name', 'string', ['length' => 150])
             ->addColumn('phone_number', 'string', ['length' => 15])
             ->addColumn('password', 'string', ['length' => 150])
-            ->addColumn('reset_code', 'string', ['length' => 16])
+            ->addColumn('reset_code', 'string', ['length' => 16, 'null' => true])
             ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
             ->create();
 
